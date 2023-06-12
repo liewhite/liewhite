cf3bd87543c92046f278d1457fa635084d1c658c
李霖 <lee@lilindeMacBook-Pro.local>
2023-06-12 14:56:48 +0800

    fix: loadconfig catch error

--- config/src/main/scala/liewhite/config/Utils.scala
+++ config/src/main/scala/liewhite/config/Utils.scala
@@ -9,18 +9,20 @@ import java.io.File
 def loadConfig[T: Schema](
   path: String = "conf/config.yaml"
 ): IO[Throwable, T] =
-  val yamlReader = new ObjectMapper(new YAMLFactory())
-  val obj        = yamlReader.readTree(new File(path))
-  val jsonWriter = new ObjectMapper()
-  val result     = jsonWriter.writeValueAsString(obj)
-  ZIO.fromEither(
-    result.fromJson[T]
-  )
-
-
-object TestConfig extends ZIOAppDefault{
-  case class A(a:Int) derives Schema
-  def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] = {
-    loadConfig[A]().debug
+  ZIO.attemptBlocking {
+    val yamlReader = new ObjectMapper(new YAMLFactory())
+    val obj        = yamlReader.readTree(new File(path))
+    val jsonWriter = new ObjectMapper()
+    val result     = jsonWriter.writeValueAsString(obj)
+    result
+  }.flatMap { str =>
+    ZIO.fromEither(
+      str.fromJson[T]
+    )
   }
+
+object TestConfig extends ZIOAppDefault {
+  case class A(a: Int) derives Schema
+  def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] =
+    loadConfig[A]().debug
 }
